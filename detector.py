f1548f1b61b79b7e13c9a1618986d8ad2c6e6fd7
Jhonata Antunes <jhonata.antunes@outlook.com>
2018-07-04 14:25:24 -0300

    Add detector methods and import fix

--- detector.py
+++ detector.py
@@ -1,4 +1,25 @@
 import torch
+from torch.autograd import Variable
+
+from src.darknet import Darknet
+from model.object import Object
+from src.preprocess import letterbox_image
+from src.util import write_results, load_classes
+
+
+def prep_image(img, inp_dim):
+    """
+    Prepare image for inputting to the neural network.
+    :param img:
+    :param inp_dim:
+    :return: Returns a Variable
+    """
+    orig_im = img
+    dim = orig_im.shape[1], orig_im.shape[0]
+    img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
+    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+    return img_, orig_im, dim
 
 
 class Detector:
@@ -70,3 +91,90 @@ class Detector:
         if type(resolution) is not str:
             raise TypeError("resolution must be a string!")
         self._resolution = resolution
+
+    def load_model(self):
+        """
+        Load the model and set its parameters
+        :return:
+        """
+        self._classes = load_classes('data/coco.names')
+        self._cuda = torch.cuda.is_available()
+
+        print("Loading network...")
+        self._model = Darknet(self._cfg)
+        self._model.load_weights(self._weights)
+        print("Network successfully loaded!")
+
+        self._model.net_info["height"] = self._resolution
+        inp_dim = int(self._model.net_info["height"])
+        assert inp_dim % 32 == 0
+        assert inp_dim > 32
+
+        if self._cuda:
+            self._model.cuda()
+
+        # self._model(get_test_input(inp_dim, CUDA), CUDA)
+        self._model.eval()
+
+    def _create_object(self, t):
+        """
+        Creates an object, that represents the detected object
+        :param t: tensor
+        :return: Object
+        """
+        x1, y1 = int(t[1]), int(t[2])
+        x2, y2 = int(t[3]), int(t[4])
+        cls = int(t[7])
+        obj = Object()
+        obj.x = x1
+        obj.y = y1
+        obj.width = x2 - x1
+        obj.height = y2 - y1
+        obj.score = float(t[6])
+        obj.label = "{0}".format(self._classes[cls])
+        return obj
+
+    def detect(self, frame):
+        """
+        Use yolov3 model to detect objects
+        :param frame: opencv frame
+        :return: list containing Object, that represents a detected object
+        """
+        if self._model is None:
+            raise ValueError("'load_model()' must be called first!")
+
+        inp_dim = int(self._model.net_info["height"])
+        img, orig_img, dim = prep_image(frame, inp_dim)
+        img_dim = torch.FloatTensor(dim).repeat(1, 2)
+
+        if self._cuda:
+            img_dim = img_dim.cuda()
+            img = img.cuda()
+
+        with torch.no_grad():
+            # tensor object
+            output = self._model(Variable(img), self._cuda)
+
+        output = write_results(output, self._confidence, self._num_classes,
+                               nms=True, nms_conf=self._nms_thresh)
+
+        if type(output) == int:
+            output = []
+        else:
+            im_dim = img_dim.repeat(output.size(0), 1)
+            scaling_factor = torch.min(inp_dim / im_dim, 1)[0].view(-1, 1)
+
+            output[:, [1, 3]] -= (inp_dim - scaling_factor * im_dim[:, 0].view(-1, 1)) / 2
+            output[:, [2, 4]] -= (inp_dim - scaling_factor * im_dim[:, 1].view(-1, 1)) / 2
+
+            output[:, 1:5] /= scaling_factor
+
+            for i in range(output.shape[0]):
+                output[i, [1, 3]] = torch.clamp(output[i, [1, 3]], 0.0, im_dim[i, 0])
+                output[i, [2, 4]] = torch.clamp(output[i, [2, 4]], 0.0, im_dim[i, 1])
+
+        object_list = []
+        for obj in output:
+            object_list.append(self._create_object(obj))
+
+        return object_list
