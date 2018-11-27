824ec4c41044b9e8cc795c79d65d79e7253a645e
Jhonata Antunes <jhonata.antunes@outlook.com>
2018-11-27 12:53:12 -0200

    Test

--- threads/object_detector.py
+++ threads/object_detector.py
@@ -45,7 +45,8 @@ class ObjectDetector(threading.Thread):
                 frame = self.vs.get_frame()
                 objects = self.detector.detect(frame['frame'])
                 if self.monitor_ip and self.monitor_port:
-                    threading.Thread(target=self._send_detection, args=(frame['frame'], objects)).start()
+                    self._send_detection(frame['frame'], objects)
+                    # threading.Thread(target=self._send_detection, args=(frame['frame'], objects)).start()
                 self.fps = 1 / (time.time() - start)
                 start = time.time()
                 self.messenger({'cam_id': self.id, 'time': frame['time'], 'objects': objects})
