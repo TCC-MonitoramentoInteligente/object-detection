3a05739e7af9aa4a41f2eb8d449d80ddb7716ece
Jhonata Antunes <jhonata.antunes@outlook.com>
2018-12-02 21:10:58 -0200

    Add analysis script

---
+++ test/alpha_beta_analysis.py
@@ -0,0 +1,35 @@
+import argparse
+import pandas
+
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def arg_parse():
+    """
+    Parse arguments
+    :return: args object
+    """
+    parser = argparse.ArgumentParser(description='Script to analyse fainting parameters')
+
+    parser.add_argument("--csv", help="Path to csv file", required=True, type=str)
+
+    return parser.parse_args()
+
+
+def main(args):
+    df = pandas.read_csv(args.csv, sep=';')
+    print(df.describe(exclude='video'))
+
+
+if __name__ == '__main__':
+    arguments = arg_parse()
+    main(arguments)
